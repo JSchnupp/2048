28887731f31b8647861749af1df97dcb6e376f35
JSchnupp <jor.schnupp@gmail.com>
2024-10-15 14:30:59 -0500

    Update array_manip.js
    updated variable names

--- js/array_manip.js
+++ js/array_manip.js
@@ -1,149 +1,93 @@
 // cody shamback
 // j00708396
 
-// FUNCTIONS TO MOVE SHIT IN ARRAY ----
+// Functions to shift numbers in array
 
-// remove abstraction
-// each of the following 4 functions shifts a given character one space in a given direction
 function shiftUp(arr, row, col) {
-    newArr = structuredClone(arr); // deep copy so we don't overwrite shit we shouldn't 
-    if (arr[row][col] === 0 || row > 3 || col > 3) return newArr; // if the square is empty or OBE, do nothing 
+    newArr = structuredClone(arr);
+    if (arr[row][col] === 0 || row > 3 || col > 3) return newArr;
 
-    currentNum = newArr[row][col]; // get current number to be moved
-
-    // check that below space exists
+    currentNum = newArr[row][col];
     if (row != 0) {
-
-        aboveNum = arr[row - 1][col]; // get number to bump into 
-
-        if (aboveNum === currentNum) { // if the two numbers colliding are the same, add them. 
+        aboveNum = arr[row - 1][col];
+        if (aboveNum === currentNum) {
             newArr[row][col] = 0;
             newArr[row - 1][col] = currentNum * 2;
-
-        } else if (aboveNum === 0) { // above space is empty - just fill the space
+        } else if (aboveNum === 0) {
             newArr[row][col] = 0;
             newArr[row - 1][col] = currentNum;
-
-        } else { // above space is not empty and also not equal to currentNum. do nothing 
-            return newArr;
         }
-
     }
-
     return newArr;
 }
 
-
 function shiftDown(arr, row, col) {
+    newArr = structuredClone(arr);
+    if (arr[row][col] === 0 || row > 3 || col > 3) return newArr;
 
-    newArr = structuredClone(arr); // deep copy so we don't overwrite shit we shouldn't 
-    if (arr[row][col] === 0 || row > 3 || col > 3) return newArr; // if the square is empty or OBE, do nothing 
-
-    currentNum = newArr[row][col]; // get current number to be moved
-
-    // check that below space exists
+    currentNum = newArr[row][col];
     if (row != 3) {
-
-        belowNum = arr[row + 1][col]; // get number to bump into 
-
-        if (belowNum === currentNum) { // if the two numbers colliding are the same, add them. 
+        belowNum = arr[row + 1][col];
+        if (belowNum === currentNum) {
             newArr[row][col] = 0;
             newArr[row + 1][col] = currentNum * 2;
-
-        } else if (belowNum === 0) { // above space is empty - just fill the space
+        } else if (belowNum === 0) {
             newArr[row][col] = 0;
             newArr[row + 1][col] = currentNum;
-
-        } else { // above space is not empty and also not equal to currentNum. do nothing 
-            return newArr;
         }
-
     }
-
     return newArr;
 }
 
 function shiftLeft(arr, row, col) {
+    newArr = structuredClone(arr);
+    if (arr[row][col] === 0 || row > 3 || col > 3) return newArr;
 
-    newArr = structuredClone(arr); // deep copy so we don't overwrite shit we shouldn't 
-    if (arr[row][col] === 0 || row > 3 || col > 3) return newArr; // if the square is empty or OBE, do nothing 
-
-    currentNum = newArr[row][col]; // get current number to be moved
-
-    // check that left space exists
+    currentNum = newArr[row][col];
     if (col != 0) {
-
-        leftNum = arr[row][col - 1]; // get number to bump into 
-
-        if (leftNum === currentNum) { // if the two numbers colliding are the same, add them. 
+        leftNum = arr[row][col - 1];
+        if (leftNum === currentNum) {
             newArr[row][col] = 0;
             newArr[row][col - 1] = currentNum * 2;
-
-        } else if (leftNum === 0) { // above space is empty - just fill the space
+        } else if (leftNum === 0) {
             newArr[row][col] = 0;
             newArr[row][col - 1] = currentNum;
-
-        } else { // above space is not empty and also not equal to currentNum. do nothing 
-            return newArr;
         }
-
     }
-
     return newArr;
 }
 
 function shiftRight(arr, row, col) {
+    newArr = structuredClone(arr);
+    if (arr[row][col] === 0 || row > 3 || col > 3) return newArr;
 
-    newArr = structuredClone(arr); // deep copy so we don't overwrite shit we shouldn't 
-    if (arr[row][col] === 0 || row > 3 || col > 3) return newArr; // if the square is empty or OBE, do nothing 
-
-    currentNum = newArr[row][col]; // get current number to be moved
-
-    // check that right space exists
+    currentNum = newArr[row][col];
     if (col != 3) {
-
-        rightNum = arr[row][col + 1]; // get number to bump into 
-
-        if (rightNum === currentNum) { // if the two numbers colliding are the same, add them. 
+        rightNum = arr[row][col + 1];
+        if (rightNum === currentNum) {
             newArr[row][col] = 0;
             newArr[row][col + 1] = currentNum * 2;
-
-        } else if (rightNum === 0) { // above space is empty - just fill the space
+        } else if (rightNum === 0) {
             newArr[row][col] = 0;
             newArr[row][col + 1] = currentNum;
-
-        } else { // above space is not empty and also not equal to currentNum. do nothing 
-            return newArr;
         }
-
     }
-
     return newArr;
 }
 
-// shifts all numbers in the array in any given direction. 
-// plug in any of thr 4 directional shift functions (above): shiftUp(), shiftDown()... 
 function shiftAllTheWay(arr, shiftFunction) {
-    newArr = structuredClone(arr); // don't overwrite shit. bad. 
-
-    // outer for loop moves everything in the chosen direction a max of 4 times to make sure it goes all the way in that direction 
-    // move each cell in "turns" so that things collide in a fair order 
+    newArr = structuredClone(arr);
     for (m = 0; m < 4; m++) {
-
-        // the following nested for loops iterate through every cell in the 2d array ONCE. 
         for (i = 0; i < arr.length; i++) {
             for (j = 0; j < arr[i].length; j++) {
                 arr = shiftFunction(arr, i, j);
             }
         }
     }
-
     return newArr;
 }
 
-// prints 2d arr so that it looks like the 2048 board
 function printInLine(arr) {
-    // size of 2048 board is always 4x4, so length is hardcoded <3
     for (let i = 0; i < 4; i++) {
         console.log(arr[i]);
     }
@@ -193,3 +137,4 @@ console.log("RIGHT");
 newArr = shiftAllTheWay(arr, shiftRight);
 printInLine(newArr);
 */
+
